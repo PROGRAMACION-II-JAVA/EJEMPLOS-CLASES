ffe8252da258406d4cfd7c8bf3f4e91b0d80aaaa
Alex P.L <111670520+Aperealazo@users.noreply.github.com>
2023-08-03 17:35:30 -0300

    Add files via upload

---
+++ ClaseArrays/src/Arrays/Ejercicio1.java
@@ -0,0 +1,18 @@
+package Arrays;
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int[] numero = new int[3];
+        //segunda forma de agregar arrays:
+       // int[] numero ={70,10,5};
+        //5 6 7
+        numero[0] = 5;
+        numero[1]=6;
+        numero[2]=7;
+     System.out.println("EL numero es:"+numero[0]);
+        
+       for(int i=0;i<3;i++){
+    System.out.println("EL numero es:"+numero[i]);
+    }
+    }
+  
+}
